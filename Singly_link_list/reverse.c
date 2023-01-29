a08aebd05bdfa2f8e339cc8a6e6e8efb721c2abc
Ajit Kumar Mishra <117336878+Ajit-kumar-mishra@users.noreply.github.com>
2023-01-29 08:13:02 +0530

    It will return reverse of SLL

---
+++ Singly_link_list/reverse.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+}*head;
+
+void createlist(int n);
+void reverselist();
+void displaylist();
+
+int main()
+{
+    int n,choice;
+
+    printf("enter the total number of nodes:\n");
+    scanf("%d",&n);
+
+    createlist(n);
+
+    printf("data in the list are:\n");
+
+    displaylist();
+
+    printf("enter 1 to reverse the list:\n");
+    scanf("%d",&choice);
+
+    if(choice==1)
+    {
+        reverselist();
+    }
+
+    printf("\nAfter reversing tha list :\n");
+
+    displaylist();
+
+    return 0;
+
+}
+
+void createlist(int n)
+{
+    struct node *newnode,*temp;
+    int data,i;
+
+    head = (struct node*)malloc(sizeof(struct node));
+
+    if(head==NULL)
+    {
+        printf("unable to allocate memory:\n");
+        exit(0);
+    }
+
+    printf("enter the data of node 1:");
+    scanf("%d",&data);
+
+    head->data = data;
+    head->next = NULL;
+
+    temp = head;
+
+    for(i=2;i<=n;i++)
+    {
+        newnode = (struct node*)malloc(sizeof(struct node));
+
+        if(newnode==NULL)
+        {
+            printf("unable to allocate memory:");
+            return;
+        }
+
+        printf("enter the data of node %d:",i);
+        scanf("%d",&data);
+
+        newnode->data = data;
+        newnode->next = NULL;
+
+        temp->next = newnode;
+        temp = temp->next;
+    }
+}
+
+void reverselist()
+{
+    struct node *curnode,*prevnode,*nextnode;
+
+    prevnode = NULL;
+
+    curnode = head;
+    nextnode = head;
+
+    while(nextnode!=NULL)
+    {
+        nextnode = nextnode->next;
+        curnode->next = prevnode;
+        prevnode = curnode;
+        curnode = nextnode;
+    }
+          head = prevnode;
+}
+
+void displaylist()
+{
+    struct node *temp;
+
+    if(head==NULL)
+    {
+        printf("list is alreay empty:\n");
+        return ;
+    }
+
+    temp = head;
+
+    while(temp!=NULL)
+    {
+        printf("Data =%d\n",temp->data);
+        temp = temp->next;
+    }
+}
