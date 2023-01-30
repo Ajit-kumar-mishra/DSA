4f39d49c752e717bd06f27d8768d863fcf8a25c7
Ajit Kumar Mishra <117336878+Ajit-kumar-mishra@users.noreply.github.com>
2023-01-30 22:10:43 +0530

    Delete first node in DLL

---
+++ Doubly_link_list/deleteatfirst.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+}*head,*last;
+
+void createlist(int n);
+void traversefromfirst();
+void deleteatfirst(int data);
+int main()
+{
+    int n;
+    int data;
+  int choice;
+    printf("enter the total number of nodes :");
+    scanf("%d",&n);
+
+    createlist(n);
+     traversefromfirst();
+
+     printf("press 1 to delete first node:\n");
+     scanf("%d",&choice);
+if(choice==1){
+    deleteatfirst(data);
+}
+
+        traversefromfirst();
+
+    return 0;
+
+}
+
+void createlist(int n)
+{
+    struct node *temp,*newnode;
+    int data,i;
+
+    head = (struct node*)malloc(sizeof(struct node));
+
+    if(head==NULL)
+    {
+        printf("unable to create memory:\n");
+        exit(0);
+    }
+
+    printf("enter the data of node 1:");
+    scanf("%d",&data);
+
+    head->data = data;
+    head->prev = NULL;
+    head->next = NULL;
+
+
+    last = head;
+
+    for(i=2;i<=n;i++)
+    {
+        newnode = (struct node*)malloc(sizeof(struct node));
+
+        if(newnode==NULL)
+        {
+            printf("unable to create meory:\n");
+            return ;
+        }
+
+        printf("enter the data of node %d:",i);
+        scanf("%d",&data);
+
+        newnode->data = data;
+        newnode->prev = last;
+        newnode->next = NULL;
+
+        last->next = newnode;
+        last = last->next; 
+    }
+}
+
+void traversefromfirst()
+{
+    struct node *temp;
+    int data;
+
+    if(head==NULL)
+    {
+        printf("list is already empty:\n");
+        return;
+    }
+    temp = head;
+    while(temp!=NULL)
+    {
+        printf("Data = %d\n",temp->data);
+        temp = temp->next;
+    }
+}
+
+void deleteatfirst(int data)
+{
+    struct node *temp,*todelete;
+
+    if(head==NULL)
+    {
+        printf("list is already empty:\n");
+        return;
+    }
+     todelete = head;
+     head = head->next;
+     if(head!=NULL)
+     
+        head->prev = NULL;
+
+        free(todelete);
+         printf("SUCCESSFULLY DELETED NODE FROM BEGINNING OF THE LIST.\n");
+}
